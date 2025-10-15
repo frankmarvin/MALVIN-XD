29c7b1fb5c0db4a651fe2427eababa7bb638d7ec
Cappy <howardfrankmev@gmail.com>
2025-10-15 23:46:43 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~xyFzXZyZ#2IxKexpZ9A8c4GS5JsYFQi_l-km1m792epet58-1y2Me45cab41e0d4bed7",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254748642275", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Nairobi" //put your country timeZone....leave blank if u don't know.
 };
